7d00ddc186418e74a51e4f81321391691775972c
Ray Everton <ray.evaf@gmail.com>
2020-05-28 23:52:44 -0300

    alteração no resource de especie e percistencia do mesmo

--- src/main/java/com/example/demo/Ds2PraticoApplication.java
+++ src/main/java/com/example/demo/Ds2PraticoApplication.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.domain.Especie;
+import com.example.demo.repositores.EspecieRepository;
+
 @SpringBootApplication
-public class Ds2PraticoApplication {
+public class Ds2PraticoApplication implements CommandLineRunner {
+	@Autowired
+	EspecieRepository especieRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ds2PraticoApplication.class, args);
+		
+	}
+	@Override
+	public void run(String... args) throws Exception{
+		Especie especie1 = new Especie(null, "Felino");
+		Especie especie2 = new Especie(null, "Canis");
+		
+		this.especieRepository.saveAll(Arrays.asList(especie1, especie2));
 	}
 
 }
--- src/main/java/com/example/demo/resource/EspecieResource.java
+++ src/main/java/com/example/demo/resource/EspecieResource.java
@@ -1,30 +1,25 @@
 package com.example.demo.resource;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Especie;
+import com.example.demo.service.EspecieService;
 
 @RestController
 @RequestMapping("/especies/{id}")
 public class EspecieResource {
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Especie> listar(){
-		Especie cat1 = new Especie(1, "cavalo");
-		Especie cat2 = new Especie(2, "cachorro");
-		Especie cat3 = new Especie(3, "Sei que não é");
-		
-		List<Especie> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		lista.add(cat3);
-		
-		return lista;
-
+	@Autowired
+	EspecieService service;
+	
+	@RequestMapping(value ="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Especie> find(@PathVariable Integer id){
+		Especie obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
