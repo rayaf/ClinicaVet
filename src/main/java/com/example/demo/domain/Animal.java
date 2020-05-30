da8d150c863592563f19e0fe7bda758aa00f68b6
Ray Everton <ray.evaf@gmail.com>
2020-05-30 11:07:53 -0300

    adicionando Serializable a classe animal

--- src/main/java/com/example/demo/domain/Animal.java
+++ src/main/java/com/example/demo/domain/Animal.java
@@ -1,5 +1,7 @@
 package com.example.demo.domain;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,7 +12,9 @@ import javax.persistence.ManyToOne;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-public class Animal {
+public class Animal implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
